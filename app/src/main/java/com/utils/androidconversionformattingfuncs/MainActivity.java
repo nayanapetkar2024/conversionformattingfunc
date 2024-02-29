cfe96eadbc34ec02c6c91523379dbed2c939b13e
nayana.petkar <nayana.petkar@intelliswift.com>
2024-02-29 15:36:00 +0530

    first commit

---
+++ app/src/main/java/com/utils/androidconversionformattingfuncs/MainActivity.java
@@ -0,0 +1,28 @@
+package com.utils.androidconversionformattingfuncs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.utils.conversionformattingfuncs.ConversionFormattingFunctions;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String date = ConversionFormattingFunctions.getFormattedDateToStringFormat();
+
+        System.out.println("date: "+date);
+
+        String newDate = ConversionFormattingFunctions.getFormattedDateDMYHMADateZone(date);
+        System.out.println("d: "+newDate);
+
+        Date dateToDateFormat = ConversionFormattingFunctions.getFormattedStringDateToDateFormat(date);
+        System.out.println("date format: "+dateToDateFormat);
+    }
+}
